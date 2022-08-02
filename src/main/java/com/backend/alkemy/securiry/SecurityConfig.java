1ca51b0c27fdaca6a9fb1c9f21fa4aef1dffc089
Augusto <augustouriona8@gmail.com>
2022-08-01 21:51:07 -0300

    BD new config

--- src/main/java/com/backend/alkemy/securiry/SecurityConfig.java
+++ src/main/java/com/backend/alkemy/securiry/SecurityConfig.java
@@ -33,11 +33,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.csrf().disable()
-                .authorizeRequests()
-                .anyRequest()
-                .authenticated().and()
-                .httpBasic()
-                .authenticationEntryPoint(entryPoint);
+                .httpBasic();
     }
 
 
